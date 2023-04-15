a72986bea0879be86a6456aeaa696fb175141493
Chavez <130843657+walterrocha101@users.noreply.github.com>
2023-04-15 16:07:34 +0300

    Add files via upload

---
+++ TGBotTestConsole/TGBotTestConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Telegram.Bot;
+
+namespace TGBotTestConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var botClient = new TelegramBotClient("715929443:AAFsK9aUiSarIl524r57_1TiwwBH093GU_I");
+
+            var me = await botClient.GetMeAsync();
+            Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+            Console.ReadLine();
+        }
+    }
+}
